b4ca8214599b50b4c7d97ffa2b868b105d7223a9
Guilherme Cadima Gonçalves <guilhermecadimagoncalves@gmail.com>
2026-09-02 10:58:52 +0100

    viewport fixed 3.0

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,6 @@
+import { useEffect } from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+
 import HomePage from './pages/HomePage';
 import StorePage from './pages/StorePage';
 import ProductPage from './pages/ProductPage';
@@ -7,13 +9,32 @@ import CheckoutSuccessPage from './pages/CheckoutSuccessPage';
 import CheckoutCancelPage from './pages/CheckoutCancelPage';
 
 export default function App() {
+
+  useEffect(() => {
+    const handlePageShow = () => {
+      window.scrollTo(0, 0);
+    };
+
+    window.addEventListener('pageshow', handlePageShow);
+
+    return () => {
+      window.removeEventListener('pageshow', handlePageShow);
+    };
+  }, []);
+
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/loja" element={<StorePage />} />
-        <Route path="/loja/sucesso" element={<CheckoutSuccessPage />} />
-        <Route path="/loja/cancelado" element={<CheckoutCancelPage />} />
+        <Route
+          path="/loja/sucesso"
+          element={<CheckoutSuccessPage />}
+        />
+        <Route
+          path="/loja/cancelado"
+          element={<CheckoutCancelPage />}
+        />
         <Route path="/loja/:slug" element={<ProductPage />} />
         <Route path="/admin" element={<AdminPage />} />
       </Routes>
